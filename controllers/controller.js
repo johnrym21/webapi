6862e7f5048dfb729782c32d6d0db028ce690e33
john-rym <j.aoun@bsynchro.com>
2021-05-28 21:36:47 +0300

    adjusted all promises error

--- controllers/controller.js
+++ controllers/controller.js
@@ -15,10 +15,10 @@ module.exports = function (app) {
         const ReceivedObject = JSON.stringify(req.body, null, 2);
         const ObjectID = req.body.quoteId;
         const QuoteType = ''; //check with rania how to determin the type of the quote
-        const BatchName = '';
-        const FileName = '';
-        const Bytes = '';
-
+        const BatchName = 'batch test 3';
+        const FileName = 'image.jpg';
+        const Bytes = "111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111101111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111100001111111111111111111111111111111111000000011111111111111111111111111111111100111101111111111111111111111111111111110001000111111111111111111110111111111111000000011111111111111111111111111111111100000001111111111111111111111111111111110000010111111111111111111111111111111111100000011111111111111111111111111111111110000001111111111111111111111111111111111000000111111111111111111111111111111111000000100111111111111111111111111111110000000110011111111111111111111111111111000010111001111111111111100000010111111111111111111111111111110100000001101111111111111111111111111111111111111111111111111111111111111111111111111111111111";
+        
         dboperations.checkIfIdExists(ObjectID).then(result => {
             if (result != null) {
                 dboperations.checkIfDestinationExists(ObjectID).then(result => {
@@ -26,72 +26,168 @@ module.exports = function (app) {
                         console.log(result)
                         res.end(result)
                     } else {
-                        (async()=>{
+                        const GetSessionId = async()=>{
                             let OpenSession = await abbyycalls.OpenSession();
-                            SessionId = OpenSession;
+                            let data = await OpenSession.sessionId;
+                            return data;
+                        };
+                        GetSessionId()
+                        .then(data => {
+                            const SessionId = data;
                             console.log(SessionId);
-                        })();
-                        (async()=>{
-                            let AddNewBatch = await abbyycalls.AddNewBatch(SessionId, BatchName);
-                            BatchId  = AddNewBatch;
-                            console.log(AddNewBatch);
-                        })();
-                        (async()=>{
-                            let OpenBatch = await abbyycalls.OpenBatch(SessionId, BatchId);
-                            console.log(OpenBatch);
-                        })();
-                        (async()=>{
-                            let AddNewDocument = await abbyycalls.AddNewDocument(SessionId, BatchId, FileName, Bytes);
-                            DocumentId = AddNewDocument;
-                            console.log(AddNewDocument);
-                        })();
-                        (async()=>{
-                            let ProcessBatch = await abbyycalls.ProcessBatch(SessionId, BatchId);
-                            console.log(ProcessBatch);
-                        })();
-                        (async()=>{
-                            let LoadDocumentResult = await abbyycalls.LoadDocumentResult(SessionId, BatchId, DocumentId, FileName);
-                            DocumentResult = LoadDocumentResult;
-                            console.log(LoadDocumentResult);
-                        })();
-                        dboperations.UpdateEnquiry(ObjectID, result, SessionId, BatchId).then( result => {
-                            result.end(result);
+                            const GetBatchId = async()=>{
+                                let AddNewBatch = await abbyycalls.AddNewBatch(SessionId, BatchName);
+                                let data  = await AddNewBatch.batchId;
+                                return data;
+                            };
+                            GetBatchId()
+                            .then(data =>{
+                                const BatchId = data;
+                                console.log(BatchId);
+                                const OpenBatch = async()=>{
+                                    let OpenBatch = await abbyycalls.OpenBatch(SessionId, BatchId);
+                                    let data  = await OpenBatch.result;
+                                    return data;
+                                };
+                                OpenBatch()
+                                .then(data => {
+                                    console.log("open batch true");
+                                    const AddNewDocument = async()=>{
+                                        let AddNewDocument = await abbyycalls.AddNewDocument(SessionId);
+                                        let data  = await AddNewDocument.documentId;
+                                        return data;
+                                    };
+                                    AddNewDocument()
+                                    .then(data => {
+                                        const DocumentId = data;
+                                        console.log(DocumentId);
+                                        const ProcessBatch = async()=>{
+                                            let ProcessBatch = await abbyycalls.ProcessBatch(SessionId);
+                                        };
+                                        ProcessBatch()
+                                        .then(data => {
+                                            console.log("batch processed");
+                                            const LoadDocumentResult = async()=>{
+                                                let LoadDocumentResult = await abbyycalls.LoadDocumentResult(SessionId);
+                                                let data = LoadDocumentResult.file;
+                                                return data;
+                                            }
+                                            LoadDocumentResult()
+                                            .then(data => {
+                                                console.log(data);
+                                                dboperations.UpdateEnquiry(ObjectID, data, SessionId, BatchId).then(
+                                                    dboperations.checkIfDestinationExists(ObjectID).then(result => {
+                                                        console.log(result)
+                                                        res.end(result)
+                                                    })
+                                                )
+                                            })
+                                            .catch(function (error) {
+                                                console.log(error);
+                                            });
+                                        })
+                                        .catch(function (error) {
+                                            console.log(error);
+                                        });
+                                    })
+                                    .catch(function (error) {
+                                        console.log(error);
+                                    });
+                                })
+                                .catch(function (error) {
+                                    console.log(error);
+                                });
+                            })
+                            .catch(function (error) {
+                                console.log(error);
+                            });
                         })
+                        .catch(function (error) {
+                                console.log(error);
+                        });
                     }
                 })
             } else {
                 dboperations.addEnquiry(ObjectID, ReceivedObject, QuoteType);
-                (async()=>{
+                const GetSessionId = async()=>{
                     let OpenSession = await abbyycalls.OpenSession();
-                    SessionId = OpenSession;
+                    let data = await OpenSession.sessionId;
+                    return data;
+                };
+                GetSessionId()
+                .then(data => {
+                    const SessionId = data;
                     console.log(SessionId);
-                })();
-                (async()=>{
-                    let AddNewBatch = await abbyycalls.AddNewBatch(SessionId, BatchName);
-                    BatchId  = AddNewBatch;
-                    console.log(AddNewBatch);
-                })();
-                (async()=>{
-                    let OpenBatch = await abbyycalls.OpenBatch(SessionId, BatchId);
-                    console.log(OpenBatch);
-                })();
-                (async()=>{
-                    let AddNewDocument = await abbyycalls.AddNewDocument(SessionId, BatchId, FileName, Bytes);
-                    DocumentId = AddNewDocument;
-                    console.log(AddNewDocument);
-                })();
-                (async()=>{
-                    let ProcessBatch = await abbyycalls.ProcessBatch(SessionId, BatchId);
-                    console.log(ProcessBatch);
-                })();
-                (async()=>{
-                    let LoadDocumentResult = await abbyycalls.LoadDocumentResult(SessionId, BatchId, DocumentId, FileName);
-                    DocumentResult = LoadDocumentResult;
-                    console.log(LoadDocumentResult);
-                })();
-                dboperations.UpdateEnquiry(ObjectID, result, SessionId, BatchId).then( result => {
-                    result.end(result);
+                    const GetBatchId = async()=>{
+                        let AddNewBatch = await abbyycalls.AddNewBatch(SessionId, BatchName);
+                        let data  = await AddNewBatch.batchId;
+                        return data;
+                    };
+                    GetBatchId()
+                    .then(data =>{
+                        const BatchId = data;
+                        console.log(BatchId);
+                        const OpenBatch = async()=>{
+                            let OpenBatch = await abbyycalls.OpenBatch(SessionId, BatchId);
+                            let data  = await OpenBatch.result;
+                            return data;
+                        };
+                        OpenBatch()
+                        .then(data => {
+                            console.log(data);
+                            const AddNewDocument = async()=>{
+                                let AddNewDocument = await abbyycalls.AddNewDocument(SessionId, BatchId, FileName, Bytes);
+                                let data  = await AddNewDocument.documentId;
+                                return data;
+                            };
+                            AddNewDocument()
+                            .then(data => {
+                                const DocumentId = data;
+                                console.log(DocumentId);
+                                const ProcessBatch = async()=>{
+                                    let ProcessBatch = await abbyycalls.ProcessBatch(SessionId, BatchId);
+                                };
+                                ProcessBatch()
+                                .then(data => {
+                                    console.log("batch processed");
+                                    const LoadDocumentResult = async()=>{
+                                        let LoadDocumentResult = await abbyycalls.LoadDocumentResult(SessionId, BatchId, DocumentId, FileName);
+                                        let data = LoadDocumentResult.file;
+                                        return data;
+                                    }
+                                    LoadDocumentResult()
+                                    .then(data => {
+                                        console.log(data);
+                                        dboperations.UpdateEnquiry(ObjectID, result, SessionId, BatchId).then(
+                                            dboperations.checkIfDestinationExists(ObjectID).then(result => {
+                                                console.log(result)
+                                                res.end(result)
+                                            })
+                                        )
+                                    })
+                                    .catch(function (error) {
+                                        console.log(error);
+                                    });
+                                })
+                                .catch(function (error) {
+                                    console.log(error);
+                                });
+                            })
+                            .catch(function (error) {
+                                console.log(error);
+                            });
+                        })
+                        .catch(function (error) {
+                            console.log(error);
+                        });
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                    });
                 })
+                .catch(function (error) {
+                        console.log(error);
+                });
             }
         })
     })
