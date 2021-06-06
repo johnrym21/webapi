3a5a61efae7b3b662a900fbc21c1988783cf2c4a
john-rym <j.aoun@bsynchro.com>
2021-06-06 21:48:15 +0300

    new route to put image directly in the imported

--- controllers/controller.js
+++ controllers/controller.js
@@ -2,7 +2,8 @@ const { json } = require('body-parser');
 const dboperations = require('./dboperations.js');
 const abbyycalls = require('./abbyycalls.js')
 const token = 'rW9QmS6XrEdfCNRFY28zfz';
-
+const fs = require('fs');
+const { database } = require('../DataBase/settings.js');
 
 module.exports = function (app) {
 
@@ -146,5 +147,58 @@ module.exports = function (app) {
                 });
             }
         })
+    })
+
+
+    app.post('/test', (req, res) => {
+
+        if (req.query.token !== token) {
+            return res.sendStatus(401);
+        }
+
+        const ReceivedObject = JSON.stringify(req.body, null, 2);
+        const quoteId = req.body.quoteId;
+        const fileName = req.body.file.Name;
+        const bytes = req.body.file.Bytes;
+        const QuoteType = ''; //check with rania how to determin the type of the quote
+        const BatchName = 'batch test 3';
+        const dir = 'cora-' + quoteId + '-' + fileName;
+        const SessionId = 123;
+        const BatchId = 123;
+        
+        dboperations.checkIfIdExists(quoteId).then( result => {
+            if (result != null) {
+                dboperations.checkIfDestinationExists(quoteId).then(result => {
+                    if (result != null) {
+                        console.log(result)
+                        res.end(result)
+                    } else {
+                        fs.mkdirSync('../import/' + dir, { recursive: true});
+
+                        fs.writeFileSync('../import/' + dir + '/' + fileName+'.jpg', base64String, {encoding: 'base64'});
+
+                        const returnJson = fs.readFileSync('../export/' + fileName+'.json', {encoding: 'base64'});
+
+                        dboperations.UpdateEnquiry(quoteId, returnJson, SessionId, BatchId);
+
+                        res.end(returnJson);
+                    }
+                })
+            } else {
+                dboperations.addEnquiry(quoteId, ReceivedObject, QuoteType);
+
+                fs.mkdirSync('../import/' + dir, { recursive: true});
+
+                fs.writeFileSync('../import/' + dir + '/' + fileName+'.jpg', base64String, {encoding: 'base64'});
+
+                const returnJson = fs.readFileSync('../export/' + fileName+'.json', {encoding: 'base64'});
+
+                dboperations.UpdateEnquiry(quoteId, returnJson, SessionId, BatchId);
+
+                res.end(returnJson);
+
+            }
+        })
+
     })
 }
