972c0955b149c380efffcc1b54f315e910419d7d
john-rym <j.aoun@bsynchro.com>
2021-06-15 21:45:42 +0300

    .

--- AbbyyCalls/parsingtests.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-var data = fs.readFileSync('my-image.png');
-console.log('sync readFile');
-console.log(data);
-console.log('break');
-var arrByte= new Uint8Array(data);
-console.log(arrByte);
